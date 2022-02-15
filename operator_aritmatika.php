75daf1397b10467bb2cae64e36de45a33e81aeeb
Muhamad Abdul Karim <87805815+abdulkaaa@users.noreply.github.com>
2022-02-15 14:58:34 +0700

    Add file via upload
    menambahkan file progres hari ke 3

---
+++ operator_aritmatika.php
@@ -0,0 +1,24 @@
+<?php
+
+echo <<<bio
+15 Februari 2022
+Muhamad Abdul Karim
+-------------------
+
+
+bio;
+
+echo "Operator Aritmatika\n...........\n";
+
+//penjumlahan
+$x = 100;
+$y = 5;
+$hasil = $x + $y;
+//var_dump($hasil);
+echo $x, " + ", $y, " = ", $hasil, "\n";
+
+//modulo / sisa bagi
+$hasil = $x % $y;
+echo $x, " % ", $y, " = ", $hasil, "\n\n";
+//var_dump($hasil);
+
---
+++ operator_perbandingan.php
@@ -0,0 +1,17 @@
+<?php
+
+echo <<<bio
+15 Februari 2022
+Muhamad Abdul Karim
+-------------------
+
+
+bio;
+
+echo "Operator Perbandingan\n...........\n";
+
+echo "Apakah '50' sama dengan 50? ", var_dump("50" == 50), "\n";
+echo "Apakah '50' dan 50 identik? ", var_dump("50" === 50), "\n";
+echo "Apakah 44 lebih besar dari 54? ", var_dump(44 > 54), "\n";
+echo "Apakah 44 lebih kecil dari atau sama dengan 54? ", var_dump(44 <= 54), "\n\n";
+
