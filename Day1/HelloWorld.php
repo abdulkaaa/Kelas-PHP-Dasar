58582855671884d89e5fa479a287752bafc840c5
Muhamad Abdul Karim <87805815+abdulkaaa@users.noreply.github.com>
2022-03-06 14:58:57 +0700

    add files via upload
    day1

---
+++ Day1/HelloWorld.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "09 Februari 2022\nMuhamad Abdul Karim\n-------------------\n";
+
+echo "Hello World!";
---
+++ Day1/tipe_data_boolean.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "09 Februari 2022\nMuhamad Abdul Karim\n-------------------\n";
+
+echo "Benar : ";
+var_dump(TruE);
+
+echo "Salah : ";
+var_dump(faLse);
