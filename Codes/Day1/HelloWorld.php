562e848b549fbac337eadfd1122c5f55249cbe31
Muhamad Abdul Karim <87805815+abdulkaaa@users.noreply.github.com>
2022-03-06 14:55:48 +0700

    delete folder codes

--- Codes/Day1/HelloWorld.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "09 Februari 2022\nMuhamad Abdul Karim\n-------------------\n";
-
-echo "Hello World!";
--- Codes/Day1/tipe_data_boolean.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "09 Februari 2022\nMuhamad Abdul Karim\n-------------------\n";
-
-echo "Benar : ";
-var_dump(TruE);
-
-echo "Salah : ";
-var_dump(faLse);
